07d6244455fd28415c181c9f6154d600f36afd1e
SofianD <doual.sofian@hotmail.com>
2021-12-17 19:30:51 +0100

    refactor server

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const { join, extname } = require('path');
 // IMPORT THIRD PARTY DEP
 const sass = require('sass');
 
-async function compileOnly(targets = undefined) {
+async function compile(targets = undefined) {
   const files = targets|| await fs.promises.readdir("sass");
-  console.log(files)
+
   for (const fileName of files) {
     const ext = extname(fileName);
 
@@ -29,33 +29,18 @@ async function compileOnly(targets = undefined) {
   return;
 }
 
-async function sassCompiler(request, response) {
+async function serverSASS(request, response) {
   if (extname(request.url) === ".html" || request.url === "/") {
-    const files = await fs.promises.readdir("sass");
-
-    for (const fileName of files) {
-      const ext = extname(fileName);
-
-      if (ext === ".sass") {
-        try {
-          await fs.promises.mkdir("css", { recursive: true });
-          
-          const compiled = await sass.compileAsync(join("sass", fileName));
-
-          const formatedfileName = join("css", fileName.replace(ext, ".css"));
-          await fs.promises.writeFile(formatedfileName, compiled.css);
-        } catch (error) {
-          console.log(error)
-        }
-      }
-    }
+    await compile();
   }
 
   fs.createReadStream(request.url.substring(1) || join('.', 'index.html')).pipe(response);
+
+  return;
 }
 
 function runServer(port = undefined) {
-  const server = http.createServer(sassCompiler);
+  const server = http.createServer(serverSASS);
 
   const defaultPort = 8000;
   server.listen(port || defaultPort);
@@ -66,7 +51,7 @@ function runServer(port = undefined) {
 function main() {
   if (process.argv[2] === "--compile") {
     const targets = process.argv.slice(3);
-    compileOnly(targets.length > 0 ? targets.map(fileName => `${fileName}.sass`) : undefined);
+    compile(targets.length > 0 ? targets.map(fileName => `${fileName}.sass`) : undefined);
   }
   else {
     const port = isNaN(Number(process.argv[2])) ? undefined : Number(process.argv[2]);
